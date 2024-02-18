54f319581fcfdbf95de2f8f76bd3ea588acd5f66
Rafazanella01 <rafazanella2004@gmail.com>
2024-02-18 17:19:09 -0300

    Update Test

--- tests/Unit/MinhaClasseAvancadoTest.php
+++ tests/Unit/MinhaClasseAvancadoTest.php
@@ -15,19 +15,35 @@ class MinhaClasseAvancadoTest extends TestCase
         $classe = new MinhaClasseAvancado();
         $classe->soma("rafa", 5);
     }
-    
+
     public function testSomaDecimais()
     {
-
+        $classe = new MinhaClasseAvancado();
+        $this->assertEquals(3, $classe->soma(1.5, 1.5));
     }
 
     public function testSomaZero()
     {
-
+        $classe = new MinhaClasseAvancado();
+        $this->assertEquals(1.5, $classe->soma(1.5, 0));
     }
 
-    public function testSubtrair()
+    public function testSubtrairInvalidos()
     {
+        $this->expectException(InvalidArgumentException::class);
+        $classe = new MinhaClasseAvancado();
+        $classe->subtracao("rafa", 5);
+    }
 
+    public function testSubtrairDecimais()
+    {
+        $classe = new MinhaClasseAvancado();
+        $this->assertEquals(0, $classe->subtracao(1.5, 1.5));
+    }
+
+    public function testSubtrairZero()
+    {
+        $classe = new MinhaClasseAvancado();
+        $this->assertEquals(1.5, $classe->subtracao(1.5, 0));
     }
 }
